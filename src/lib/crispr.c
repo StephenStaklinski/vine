d22737e44a25b54f446bad1b846b5f002ab2fe64
Adam Siepel <asiepel@cshl.edu>
2026-01-04 11:21:37 -0500

    more efficient zeroing of matrices. Makes huge difference with crispr-modtype GLOBAL

--- src/lib/crispr.c
+++ src/lib/crispr.c
@@ -716,8 +716,15 @@ void cpr_set_branch_matrix(MarkovMatrix *P, double t, double silent_rate, Vector
   double exp_t_sil = exp(-t * silent_rate),
     one_min_exp_t_sil = 1 - exp_t_sil,
     exp_t_sil_one_min_exp_t = exp_t_sil * (1 - exp(-t));
-  /*  mat_zero(P->matrix);*/
   assert(mutrates->size == P->size-1);
+
+  /* this allows us to avoid resetting zeroes each time, which gets
+     expensive with large matrices */
+  static int silst_prev = -1;
+  if (silst != silst_prev) {
+    mat_zero(P->matrix);
+    silst_prev = silst;
+  }
   
   /* substitution probabilities from 0 (unedited) state to all edited
      (and not silent) states */
@@ -726,13 +733,9 @@ void cpr_set_branch_matrix(MarkovMatrix *P, double t, double silent_rate, Vector
   mm_set(P, 0, 0, exp(-t*(1+silent_rate)));
   
   /* substitution probabilities from edited states to themselves */
-  for (j = 1; j < silst; j++) {
+  for (j = 1; j < silst; j++) 
     mm_set(P, j, j, exp_t_sil);
-    for (int i = 1; i < j; i++) {  /* all others zero */
-      mm_set(P, i, j, 0);
-      mm_set(P, j, i, 0);
-    }
-  }
+    /* leave all others zero */
   
   /* substitution probabilities to silent state */
   for (j = 0; j < silst; j++)
@@ -748,8 +751,15 @@ void cpr_branch_grad(Matrix *grad, double t, double silent_rate, Vector *mutrate
   double es = exp(-t * silent_rate);
   double A  = (silent_rate * es * em1 + exp(-t * (1+silent_rate)));
   double B = silent_rate * es; 
-  mat_zero(grad);
-         
+
+  /* this allows us to avoid resetting zeroes each time, which gets
+     expensive with large matrices */
+  static int silst_prev = -1;
+  if (silst != silst_prev) {
+    mat_zero(grad);
+    silst_prev = silst;
+  }
+  
   /* derivatives of substitution probabilities from 0 (unedited) state
      to all edited (and not silent) states */
   for (j = 1; j < silst; j++)
@@ -773,8 +783,15 @@ void cpr_silent_rate_grad(Matrix *grad, double t, double silent_rate, Vector *mu
   double Es = -t * exp(-silent_rate * t);
   double E1 = exp(-t);
   double A = Es * (1.0-E1);
-  /*  mat_zero(grad);*/
-         
+
+  /* this allows us to avoid resetting zeroes each time, which gets
+     expensive with large matrices */
+  static int silst_prev = -1;
+  if (silst != silst_prev) {
+    mat_zero(grad);
+    silst_prev = silst;
+  }
+  
   /* derivatives of substitution probabilities from 0 (unedited) state
      to all edited (and not silent) states */
   for (j = 1; j < silst; j++)
@@ -783,13 +800,9 @@ void cpr_silent_rate_grad(Matrix *grad, double t, double silent_rate, Vector *mu
   
   /* derivatives of substitution probabilities from edited states to
      themselves */
-  for (j = 1; j < silst; j++) {
+  for (j = 1; j < silst; j++) 
     mat_set(grad, j, j, Es);
-    for (int i = 1; i < j; i++) {
-      mat_set(grad, i, j, 0);
-      mat_set(grad, j, i, 0);
-    }
-  }
+    /* leave all others zero */
 
   /* derivatives of substitution probabilities to silent state */
   for (j = 0; j < silst; j++)
